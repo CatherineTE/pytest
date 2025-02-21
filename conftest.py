332cae8de52afe75c7a01a31f21e04232b60e152
CatherineTE <72939295+CatherineTE@users.noreply.github.com>
2025-02-21 18:48:56 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -9,6 +9,7 @@ def browser():
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
     browser = webdriver.Chrome(options=options)
     browser.implicitly_wait(5)
     yield browser
